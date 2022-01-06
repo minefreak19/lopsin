b7991182c9016c5be922a4656bb70648dd9a18d6
minefreak19 <61090340+minefreak19@users.noreply.github.com>
2022-01-06 19:30:33 +0530

    add buffer.h

---
+++ src/common/buffer.h
@@ -0,0 +1,154 @@
+/////////////////
+// HEADER PART //
+/////////////////
+
+#ifndef BUFFER_H_
+#define BUFFER_H_
+
+#include <stddef.h>
+
+#include "./util.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+#define DEFAULT_BUFFER_CAP 1024
+
+typedef struct Buffer {
+    char *data;
+    size_t size;
+    size_t cap;
+} Buffer;
+
+Buffer *new_buffer(size_t cap);
+
+void buffer_clear(Buffer *);
+void buffer_free(Buffer *);
+
+void buffer_append_char(Buffer *, char);
+void buffer_append_str(Buffer *, const char *str, size_t len);
+void buffer_append_cstr(Buffer *, const char *cstr);
+
+void buffer_append_fmt(Buffer *, const char *format, ...) PRINTF_FORMAT(2, 3);
+
+void buffer_rewind(Buffer *, size_t prev_sz);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* BUFFER_H_ */
+
+/////////////////
+// SOURCE PART //
+/////////////////
+#ifdef BUFFER_IMPLEMENTATION
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "util.h"
+
+static void buffer_ensure(Buffer *buf, size_t req)
+{
+    if (buf->cap >= req) return;
+
+    buf->data = NOTNULL(realloc(buf->data, req));
+}
+
+Buffer *new_buffer(size_t cap)
+{
+    if (cap == 0) cap = DEFAULT_TMP_BUF_CAP;
+
+    Buffer *result = NOTNULL(malloc(sizeof(Buffer)));
+
+    *result = (Buffer) {
+        .cap = cap,
+        .data = NOTNULL(calloc(cap, sizeof(char))),
+        .size = 0,
+    };
+
+    return result;
+}
+
+void buffer_clear(Buffer *buf) 
+{
+    assert(buf != NULL);
+    buf->size = 0;
+}
+
+void buffer_free(Buffer *buf)
+{
+    assert(buf != NULL);
+
+    if (buf->size > 0) {
+        PANIC("Attempted to free non-empty buffer. buffer_clear() should be called first.\n");
+    }
+
+    free(buf->data);
+    free(buf);
+}
+
+void buffer_append_char(Buffer *buf, char c)
+{
+    buffer_ensure(buf, buf->size + 1);
+    buf->data[buf->size++] = c;
+}
+
+void buffer_append_str(Buffer *buf, const char *str, size_t len)
+{
+    assert(str != NULL);
+    assert(buf != NULL);
+
+    if (len == 0) return;
+    
+    buffer_ensure(buf, buf->size + len);
+
+    memcpy(&buf->data[buf->size], str, len);
+    buf->size += len;
+}
+
+void buffer_append_cstr(Buffer *buf, const char *cstr)
+{
+    assert(cstr != NULL);
+    buffer_append_str(buf, cstr, strlen(cstr));
+}
+
+void buffer_append_fmt(Buffer *buf, const char *format, ...)
+{
+    assert(buf != NULL);
+    assert(format != NULL);
+
+    va_list vargs;
+    va_start(vargs, format);
+
+    // grab an estimate
+    size_t len = vsnprintf(NULL, 0, format, vargs); 
+
+    va_end(vargs);
+
+    char *str = NOTNULL(malloc(len * sizeof(char)));
+
+    va_start(vargs, format);
+    // actually do the thing
+    vsnprintf(str, len + 1, format, vargs);
+
+    buffer_append_cstr(buf, str);
+
+    free(str);
+    va_end(vargs);
+}
+
+void buffer_rewind(Buffer * buf, size_t prev_sz)
+{
+    assert(buf != NULL);
+    assert(prev_sz <= buf->size);
+    
+    buf->size = prev_sz;
+}
+
+#endif // BUFFER_IMPLEMENTATION
--- src/common/util.h
+++ src/common/util.h
@@ -14,6 +14,12 @@ extern "C"
 
 #define ARRAY_LEN(arr) (sizeof(arr) / sizeof((arr)[0]))
 
+#if defined(__GNUC__) || defined(__clang__)
+# define PRINTF_FORMAT(fmt_idx, chk_idx) __attribute__ ((format (printf, fmt_idx, chk_idx)))
+#else
+# define PRINTF_FORMAT(...)
+#endif // defined(__GNUC__) || defined(__clang__)
+
 #ifdef _DEBUG
 # define NOTNULL(ptr) notnull_impl(__FILE__, __LINE__, __func__, #ptr, ptr)
 #else
