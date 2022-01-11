25cc88b5f985cdaf71faeeeda09242d04abe8f73
minefreak19 <61090340+minefreak19@users.noreply.github.com>
2022-01-11 13:22:41 +0530

    add common/lopsin.h

---
+++ src/common/lopsin.h
@@ -0,0 +1,19 @@
+/*
+Created 08 January 2022
+ */
+
+#ifndef LOPSIN_H_
+#define LOPSIN_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+#include "../lopsinvm/lopsinvm.h"
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* LOPSIN_H_ */
