1d4ffe7c76c006962c62db6c30f3b46a698b599e
minefreak19 <61090340+minefreak19@users.noreply.github.com>
2022-01-19 19:01:23 +0530

    [lopsinvm] add `--debug` command-line option

--- src/lopsinvm/main.c
+++ src/lopsinvm/main.c
@@ -3,12 +3,18 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "util.h"
 
+#define cstreq(a, b) (strcmp(a, b) == 0)
+
 static void usage(FILE *stream, const char *program)
 {
-    fprintf(stream, "USAGE: %s <input.lopsinvm>\n", program);
+    fprintf(stream, "USAGE: %s <input.lopsinvm> [OPTIONS]\n", program);
+    fprintf(stream, 
+        "OPTIONS:\n"
+        "   --debug, -d             Enable debug mode\n");
 }
 
 int main(int argc, const char **argv)
@@ -19,16 +25,37 @@ int main(int argc, const char **argv)
 
     const char *program_name = *argv++;
     
-    const char *input_file = *argv++;
-    if (input_file == NULL) {
+    struct {
+        const char *input_file;
+        bool debug_mode;
+    } args = {0};
+
+    while (*argv != NULL) {
+        const char *arg = *argv++;
+        
+        if (cstreq(arg, "--debug") || cstreq(arg, "-d")) {
+            args.debug_mode = true;
+        } else {
+            // throw error if we already have an input file
+            if (args.input_file != NULL) {
+                usage(stderr, program_name);
+                fprintf(stderr, "ERROR: Unknown option `%s`\n", arg);
+                exit(1);
+            }
+
+            args.input_file = arg;
+        }
+    }
+
+    if (args.input_file == NULL) {
         usage(stderr, program_name);
         fprintf(stderr, "ERROR: No input file provided\n");
         exit(1);
     }
-
     LopsinVM vm = lopsinvm_new();
+    vm.debug_mode = args.debug_mode;
 
-    lopsinvm_load_program_from_file(&vm, input_file);
+    lopsinvm_load_program_from_file(&vm, args.input_file);
 
     lopsinvm_start(&vm);
 
